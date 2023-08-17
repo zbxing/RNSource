3a45524b26ea4b5131c30394832b6f92348344e6
zbxing <zoubangxing@126.com>
2023-08-17 15:21:15 +0800

    重置提交

--- android/app/src/main/java/com/rnsource/MainActivity.java
+++ android/app/src/main/java/com/rnsource/MainActivity.java
@@ -1,5 +1,12 @@
 package com.rnsource;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -28,5 +35,36 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setNavigationBarStatusBarTranslucent(this);
+  }
+
+  /**
+   * 设置状态栏和系统导航条透明，用于APP全屏显示
+   * @param activity
+   */
+  public static void setNavigationBarStatusBarTranslucent(Activity activity){
+    // 从Android-11(API-level-30)开始，setSystemUiVisibility废弃了
+    var window = activity.getWindow();
+
+    //请求应用显示在系统界面后面
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      window.setDecorFitsSystemWindows(false);
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      window.setNavigationBarContrastEnforced(false);
+    }
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    window.setNavigationBarColor(Color.TRANSPARENT);
+
+    //设置导航栏按钮或导航条颜色
+//    setNavigationBarBtnColor(window, NightMode.isNightMode(window.context))
   }
 }
